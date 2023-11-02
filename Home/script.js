c944fb077a0d82653a4e8d72355a8c596e73ae23
Ewixxx <130546265+Ewixxx@users.noreply.github.com>
2023-11-02 08:38:23 +0800

    update

--- Home/script.js
+++ Home/script.js
@@ -1,63 +1,19 @@
-let tabs = document.querySelectorAll(".tab");
-let tabsContent = document.querySelectorAll(".content");
+ // Get all the scroll links
+ const scrollLinks = document.querySelectorAll('.scroll-link');
 
-tabs.forEach((tab, index) => {
-  tab.addEventListener("click", (e) => {
-    tabs.forEach((tab) => {
-      tab.classList.remove("active");
-    });
+ // Add click event listeners to scroll smoothly to the target section
+ scrollLinks.forEach(link => {
+   link.addEventListener('click', (e) => {
+     e.preventDefault();
+     const targetId = link.getAttribute('href').substring(1);
+     const targetSection = document.getElementById(targetId);
 
-    tab.classList.add("active");
+     if (targetSection) {
+       targetSection.scrollIntoView({ behavior: 'smooth' });
+     }
+   });
+ });
 
-    tabsContent.forEach((content) => {
-      content.classList.remove("active");
-    });
-    tabsContent[index].classList.add("active");
-
-    let line = document.querySelector(".line");
-    line.style.width = e.target.offsetWidth + "px";
-    line.style.left = e.target.offsetLeft + "px";
-  });
-});
-
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("openModal");
-var closeBtn = document.getElementById("closeModal");
-
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-closeBtn.onclick = function() {
-    modal.style.display = "none";
-}
-
-// Close the modal if the user clicks outside of it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
-
-  alertPlaceholder.append(wrapper)
-}
-
-const alertTrigger = document.getElementById('liveAlertBtn')
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    appendAlert('<a href="https://github.com/Ewixxx/FlashCard"> Login with Register and update profile PHP & SQL &HorizontalLine; click</a>', 'success')
-  })
-}
 
 
 const myCarouselElement = document.querySelector('#carouselExampleDark')
@@ -66,3 +22,20 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2000,
   touch: false
 })
+
+
+function downloadCV() {
+  // Replace 'your_cv.pdf' with the actual filename and extension of your CV.
+  var cvFilename = 'Eric Reyes-CV.docx';
+  
+  // Create an anchor element to trigger the download.
+  var downloadLink = document.createElement('a');
+  downloadLink.setAttribute('href', cvFilename);
+  downloadLink.setAttribute('download', cvFilename);
+
+  // Simulate a click on the anchor element to initiate the download.
+  downloadLink.style.display = 'none';
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
